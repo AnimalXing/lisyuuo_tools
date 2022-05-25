d63e6328194c7a5cbbe6a0f33a2514affced484c
AnimalXing <66161458+AnimalXing@users.noreply.github.com>
2022-05-24 21:25:13 -0400

    Create character_occurence.py

---
+++ character_occurence.py
@@ -0,0 +1,27 @@
+"""find characters from a file with the most occurence"""
+import collections
+import matplotlib.pyplot as plt
+
+
+plt.rcParams['font.family'] = ['Heiti TC']#to have cjk character shown correctly, we have to change the font
+f = open("trad_v1.txt", "r")
+string = ''
+for name in f:
+    string+=name[0:len(name)-1]
+c = collections.Counter(string)
+d = dict(c)
+l1 = []
+l2 = []
+for i in d:
+    if d[i]> 15:
+        l1.append(i)
+        l2.append(d[i])
+
+
+
+plt.bar(l1,l2)
+plt.show()
+
+
+plt.bar(l1, l2)
+plt.show()
